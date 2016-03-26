8b6c2e0d4ed5770f10092c93075e6bc2b5b1ced7
Rhoboholics <uplockton@gmail.com>
2016-03-26 12:25:10 +0200

    Update assignment7.ino

--- assignment7.ino
+++ assignment7.ino
@@ -11,41 +11,49 @@
 
 Odometer encoderLeft(93), encoderRight(93); //Βάλτε τους δικούς σας παλμούς ανά μέτρο
 Gyroscope gyro(18); //Βάλτε την κατάλληλη τιμή σύμφωνα με το γυροσκόπιό σας
-Car folkracer;
-boolean state1Done = false;
-boolean state2Done = false;
+Car folkracer;//Το όνομα του αυτοκινήτου
+boolean state1Done = false;//Η κατάσταση του 1Done είναι false(0)
+boolean state2Done = false;//Η κατάσταση του 2Done είναι false(0)
 
 void setup() {
-  gyro.attach();
+  gyro.attach(18);//?
   encoderLeft.attach(3); //Χρησιμοποιήστε τα σωστά pins!
-  encoderRight.attach(2);
-  encoderLeft.begin();
-  encoderRight.begin();
-  gyro.begin();
-  folkracer.begin(encoderLeft, encoderRight, gyro);
-  folkracer.enableCruiseControl();
-  folkracer.setSpeed(0.5);
+  encoderRight.attach(2);//?
+  encoderLeft.begin(93);//?
+  encoderRight.begin(93);//?
+  gyro.begin(18);//?
+  folkracer.begin(encoderLeft, encoderRight, gyro);//το αμάξι λειτουργεί με τα encoderLeft, encoderRight, gyro
+  folkracer.enableCruiseControl();//?
+  folkracer.setSpeed(0.5);//το αμάξι έχει ταχύτητα 0,5
 }
 
 void loop() {
-  folkracer.updateMotors();
-  unsigned long distance = encoderLeft.getDistance();
-  distance = distance + encoderRight.getDistance();
-  if (distance > 20) {
-    if (distance < 30) {
-      if (state1Done == false) {
-        folkracer.stop();
-        folkracer.rotate(180);
-        folkracer.setSpeed(0.5);
-        state1Done = true;
+  folkracer.updateMotors();//?
+  unsigned long distance = encoderLeft.getDistance();//?
+  distance = distance + encoderRight.getDistance();//?
+  if (distance > 20) {//εαν η απόσταση που εχει διανυσει είναι >20
+    if (distance < 30) {//εαν η απόσταση εχει διανυσει είναι <30 τότε:
+      if (state1Done == false) {//εαν το 1Done είναι false(0) 
+        folkracer.stop();//Το αμάξι σταματάει
+        folkracer.rotate(180);//το αμάξι στριβει 180 μοίρες
+        folkracer.setSpeed(0.5);//το αμάξι εχει ταχύτητα 0,5
+        state1Done = true;//το 1Done ειναι true(1)
       }
     }
   }
   //TO-DO
+  if(distance==80||90){
+   folkracer.stop();
+   folkracer.rotate(90);
+   folkracer.setSpeed(0,5)
+  }
   //Όταν το αυτοκινητάκι έχει διανύσει από 80 μέχρι 90 εκατοστά, τότε σταματήστε το και μετά περιστρέψτε το κατά 90 μοίρες
   //στα δεξιά. Στη συνέχεια, θέστε ταχύτητα 0.5 μέτρα το δευτερόλεπτο
   
   //TO-DO
-  //Όταν το αυτοκινητάκι έχει διανύσει απόσταση μεγαλύτερη των 150 εκατοστών, τότε πείτε του να σταματήσει (ή εναλλακτικά θέστε
+  if(distance >150){
+   folkracer.stop()||state2Done=false;
+  }
+  //Όταν το αυτοκινητάκι έχει διανύσει απόσταση μεγαλύτερη των 150 εκατοστών, τότε πείτε του να σταματήσει (ή εναλλακτικά θέστε       
   //ταχύτητα μηδέν)
 }
